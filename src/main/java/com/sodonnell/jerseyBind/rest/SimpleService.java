111d012208ae06b6188f768ba09591cb8c2cdffe
S O'Donnell <sodonnell@cloudera.com>
2021-03-10 13:46:34 +0000

    Remove javax.inject

--- src/main/java/com/sodonnell/jerseyBind/rest/SimpleService.java
+++ src/main/java/com/sodonnell/jerseyBind/rest/SimpleService.java
@@ -11,7 +11,7 @@ import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 @Path("/")
 public class SimpleService {
